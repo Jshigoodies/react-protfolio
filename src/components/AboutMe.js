546f18496d02290b51f235fa2fa24d7f677db838
Jason Shi <jshigoodies@gmail.com>
2023-04-24 00:36:05 -0500

    added projects to my portfolio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,14 @@ function AboutMe() {
       <div className='aboutmeDiv'>
         <h1>About Me</h1>
         <img className="avatar-img" src={avatar} alt="Avatar Image"/>
-        <p>About me goes here :D</p>
+        <p> 
+            Hi, I'm Jason Shi, a recent Computer Science graduate with a passion for volunteering and hobbies. 
+            I've volunteered with various organizations, including the Galveston Food Bank and nursing/retirement homes. 
+            I'm skilled in programming languages like Java, Python, HTML, CSS, and JavaScript. 
+            In my free time, I enjoy playing musical instruments, 
+            practicing sports, traveling, fishing, and working out. 
+            You can find some of my work on my GitHub, Itch.io, YouTube, and personal website.
+        </p>
       </div>
     );
   }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo from '../assets/logo192.png'
+import tts from '../assets/text-to-speech.png'
 
 function Portfolio() {
     return (
@@ -6,13 +8,43 @@ function Portfolio() {
         <h1>Portfolio</h1>
         <ul>
           <li>
-            <a href="https://example.com/project1">Project 1</a>
+            <div className='project-item'>
+                <h3>React Portfolio Project</h3>
+                <p>The current one you are on right now. :)</p>
+                <img src={logo} alt='project image' />
+                <a href="https://github.com/Jshigoodies/react-protfolio">React Portfolio Repo</a> 
+            </div>
           </li>
           <li>
-            <a href="https://example.com/project2">Project 2</a>
+            <div className='project-item'>
+                <h3>World's Hardest Game in 3D remake</h3>
+                <p>It is a game that I created ind 3D. Reference is from the coolmath games.</p>
+                <img src='https://img.itch.zone/aW1nLzEwNjE0MTQyLnBuZw==/347x500/kVZVJp.png' alt='game image'/>
+                <a href="https://github.com/Jshigoodies/World-s-Hardest-Game-3D">World's Hardest Game Remake Repo</a>
+                <br/>
+                <br/>
+                <a href='https://jshigoodies.itch.io/worlds-hardest-game-in-3'>Play my game here</a>
+            </div>
+            
           </li>
           <li>
-            <a href="https://example.com/project3">Project 3</a>
+            <div className='project-item'>
+                <h3>Python Audio Text and Speech</h3>
+                <p>It is a basic program in python that takes in audio from any microphone and allows you to command it to open browsers and automatically login to websites</p>
+                <img className='ttsImg' src={tts} alt='text to speech image' />
+                <a href="https://github.com/Jshigoodies/Py_audio">Py_audio Repo</a> 
+            </div>
+          </li>
+          <li>
+            <div className='project-item'>
+                <h3>JDA and Minecraft</h3>
+                <p>It is an api that allows minecraft and discord to interact with eachother. Specifically through messages</p>
+                <img src='https://static.spigotmc.org/img/spigot-og.png' alt='spigot image'/>
+                <a href='https://github.com/Jshigoodies/Maven_JDA_and_Spigot'>JDA and Spigot Repo</a>
+                <br/>
+                <br/>
+                <a href='https://www.youtube.com/watch?v=UV62Ks9kzTY'>youtube video</a>
+            </div>
           </li>
         </ul>
       </div>
